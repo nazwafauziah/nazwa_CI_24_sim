fa9c3b5b80db3c378db9e3f171d256b59c3b501a
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-04-14 22:52:33 +0700

    Update buku_model.php

--- application/models/buku_model.php
+++ application/models/buku_model.php
@@ -1,36 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+class buku_model extends CI_Model {
 
-class Buku_model extends CI_Model {
-
-    public function get_all()
-    {
-        $this->db->select('buku.*, kategori.nama_kategori');
+    public function get_all(){
+        $this->db->select('buku.*, kategori.nama_kategori as nama_kategori');
         $this->db->from('buku');
-        $this->db->join('kategori', 'kategori.id_kategori = buku.id_kategori', 'left');
+        $this->db->join('kategori', 'kategori.id = buku.kategori_id');
         return $this->db->get()->result();
     }
 
-    public function insert($data)
-    {
-        $this->db->insert('buku', $data);
+    public function get_kategori(){
+        return $this->db->get('kategori')->result();
     }
 
-    public function get_by_id($id)
-    {
-        return $this->db->get_where('buku', ['id_buku' => $id])->row();
+    public function insert($data){
+        return $this->db->insert('buku', $data);
     }
 
-    public function update($id, $data)
-    {
-        $this->db->where('id_buku', $id);
-        $this->db->update('buku', $data);
+    public function get_by_id($id){
+        return $this->db->get_where('buku',['id'=>$id])->row();
     }
 
-    public function delete($id)
-    {
-        $this->db->where('id_buku', $id);
-        $this->db->delete('buku');
+    public function update($id,$data){
+        $this->db->where('id',$id);
+        return $this->db->update('buku',$data);
+    }
+
+    public function delete($id){
+        return $this->db->delete('buku',['id'=>$id]);
     }
 }
-?>
