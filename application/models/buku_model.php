23b83bda91e0b4e05a5e5dbe02335abce1650c42
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-04-15 00:02:57 +0700

    Update buku_model.php

--- application/models/buku_model.php
+++ application/models/buku_model.php
@@ -1,57 +1,32 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+class Buku_model extends CI_Model {
 
-class buku_model extends CI_Model {
-
-    // GET ALL DATA BUKU WITH JOIN KATEGORI
-    public function get_all()
-    {
-        $this->db->select('buku.*, kategori.nama_kategori as nama_kategori');
-        $this->db->from('buku');
-        $this->db->join('kategori', 'kategori.id = buku.kategori_id', 'left');
-        $this->db->order_by('buku.id', 'DESC');
-        return $this->db->get()->result();
-    }
-
-    // GET ALL KATEGORI (untuk dropdown)
-    public function get_kategori()
-    {
+    public function get_kategori() {
         return $this->db->get('kategori')->result();
     }
 
-    // INSERT DATA BUKU
-    public function insert($data)
-    {
-        return $this->db->insert('buku', $data);
+    public function insert($data) {
+        $this->db->insert('buku', $data);
     }
 
-    // GET BUKU BY ID
-    public function get_by_id($id)
-    {
-        return $this->db->get_where('buku', ['id' => $id])->row();
+    public function get_all() {
+    $this->db->select('buku.*, kategori.nama_kategori');
+    $this->db->from('buku');
+    $this->db->join('kategori', 'kategori.id = buku.id_kategori', 'left');
+    return $this->db->get()->result();
     }
 
-    // UPDATE DATA BUKU
-    public function update($id, $data)
-    {
+    public function delete($id) {
+    $this->db->where('id', $id);
+    $this->db->delete('buku');
+    }
+
+    public function get_by_id($id) {
+    return $this->db->get_where('buku', ['id' => $id])->row();
+    }
+
+    public function update($id, $data) {
         $this->db->where('id', $id);
-        return $this->db->update('buku', $data);
-    }
-
-    // DELETE DATA BUKU
-    public function delete($id)
-    {
-        $this->db->where('id', $id);
-        return $this->db->delete('buku');
-    }
-
-    // CEK KODE BUKU UNIQUE
-    public function cek_kode_buku($kode_buku, $id = null)
-    {
-        $this->db->where('kode_buku', $kode_buku);
-        if ($id) {
-            $this->db->where('id !=', $id);
-        }
-        return $this->db->get('buku')->num_rows() > 0;
+        $this->db->update('buku', $data);
     }
 }
