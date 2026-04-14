38f360c10fa6ef12ab5d30b34c2acfc488fec9db
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-04-15 00:06:46 +0700

    Update buku.php

--- application/controllers/buku.php
+++ application/controllers/buku.php
@@ -1,70 +1,78 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Buku extends CI_Controller {
 
-    public function __construct() {
+    public function __construct(){
         parent::__construct();
-        $this->load->model('Buku_model');
+        $this->load->model('buku_model');
     }
 
-    // tampil form
-    public function tambah() {
-        $data['kategori'] = $this->Buku_model->get_kategori();
-        $this->load->view('buku_form', $data);
+    public function index(){
+        $data['buku'] = $this->buku_model->get_all();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('buku/index', $data);
+        $this->load->view('templates/footer');
     }
 
-    // proses simpan
-    public function simpan() {
-    $data = [
-        'kode_buku' => $this->input->post('kode_buku'),
-        'judul' => $this->input->post('judul'),
-        'penulis' => $this->input->post('penulis'),
-        'penerbit' => $this->input->post('penerbit'),
-        'tahun' => $this->input->post('tahun'),
-        'id_kategori' => $this->input->post('kategori'),
-        'stok' => $this->input->post('stok'),
-        'lokasi_rak' => $this->input->post('lokasi_rak'),
-    ];
+    public function tambah(){
+        $data['kategori'] = $this->buku_model->get_kategori();
 
-    // SIMPAN DATA
-    $this->Buku_model->insert($data);
-
-    // REDIRECT
-    redirect('buku');
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('buku/tambah', $data);
+        $this->load->view('templates/footer');
     }
 
-    public function index() {
-    $data['buku'] = $this->Buku_model->get_all();
-    $this->load->view('buku_list', $data);
+    public function simpan(){
+        $data = [
+            'kode_buku' => $this->input->post('kode_buku'),
+            'judul_buku' => $this->input->post('judul_buku'),
+            'penulis' => $this->input->post('penulis'),
+            'penerbit' => $this->input->post('penerbit'),
+            'tahun' => $this->input->post('tahun'),
+            'kategori_id' => $this->input->post('kategori_id'),
+            'stok' => $this->input->post('stok'),
+            'lokasi_rak' => $this->input->post('lokasi_rak')
+        ];
+
+        $this->buku_model->insert($data);
+        redirect('buku');
     }
 
-    public function hapus($id) {
-    $this->Buku_model->delete($id);
-    redirect('buku');
+    public function edit($id){
+        $data['buku'] = $this->buku_model->get_by_id($id);
+        $data['kategori'] = $this->buku_model->get_kategori();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('buku/edit', $data);
+        $this->load->view('templates/footer');
     }
 
-    public function edit($id) {
-    $data['buku'] = $this->Buku_model->get_by_id($id);
-    $data['kategori'] = $this->Buku_model->get_kategori();
-    $this->load->view('buku_edit', $data);
-    }
-
-    public function update() {
+    public function update(){
         $id = $this->input->post('id');
 
         $data = [
             'kode_buku' => $this->input->post('kode_buku'),
-            'judul' => $this->input->post('judul'),
+            'judul_buku' => $this->input->post('judul_buku'),
             'penulis' => $this->input->post('penulis'),
             'penerbit' => $this->input->post('penerbit'),
             'tahun' => $this->input->post('tahun'),
-            'id_kategori' => $this->input->post('kategori'),
+            'kategori_id' => $this->input->post('kategori_id'),
             'stok' => $this->input->post('stok'),
-            'lokasi_rak' => $this->input->post('lokasi_rak'),
+            'lokasi_rak' => $this->input->post('lokasi_rak')
         ];
 
-        $this->Buku_model->update($id, $data);
+        $this->buku_model->update($id,$data);
+        redirect('buku');
+    }
+
+    public function hapus($id){
+        $this->buku_model->delete($id);
         redirect('buku');
     }
 }
