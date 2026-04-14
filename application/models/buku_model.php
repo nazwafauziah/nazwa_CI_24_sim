cd0829d72db34689e0d896c07df2d516e8aa0434
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-04-14 23:48:11 +0700

    Update buku_model.php

--- application/models/buku_model.php
+++ application/models/buku_model.php
@@ -3,35 +3,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class buku_model extends CI_Model {
 
-    public function get_all(){
+    // GET ALL DATA BUKU WITH JOIN KATEGORI
+    public function get_all()
+    {
         $this->db->select('buku.*, kategori.nama_kategori as nama_kategori');
         $this->db->from('buku');
         $this->db->join('kategori', 'kategori.id = buku.kategori_id', 'left');
+        $this->db->order_by('buku.id', 'DESC');
         return $this->db->get()->result();
     }
 
-    public function get_kategori(){
+    // GET ALL KATEGORI (untuk dropdown)
+    public function get_kategori()
+    {
         return $this->db->get('kategori')->result();
     }
 
-    public function insert($data){
+    // INSERT DATA BUKU
+    public function insert($data)
+    {
         return $this->db->insert('buku', $data);
     }
 
-    // PERBAIKAN: get_by_id menggunakan 'id' (karena primary key di tabel buku adalah 'id')
-    public function get_by_id($id){
+    // GET BUKU BY ID
+    public function get_by_id($id)
+    {
         return $this->db->get_where('buku', ['id' => $id])->row();
     }
 
-    // PERBAIKAN: update menggunakan 'id'
-    public function update($id, $data){
+    // UPDATE DATA BUKU
+    public function update($id, $data)
+    {
         $this->db->where('id', $id);
         return $this->db->update('buku', $data);
     }
 
-    // PERBAIKAN: delete menggunakan 'id'
-    public function delete($id){
+    // DELETE DATA BUKU
+    public function delete($id)
+    {
         $this->db->where('id', $id);
         return $this->db->delete('buku');
+    }
+
+    // CEK KODE BUKU UNIQUE
+    public function cek_kode_buku($kode_buku, $id = null)
+    {
+        $this->db->where('kode_buku', $kode_buku);
+        if ($id) {
+            $this->db->where('id !=', $id);
+        }
+        return $this->db->get('buku')->num_rows() > 0;
     }
 }
