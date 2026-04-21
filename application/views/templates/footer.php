4b0c7daa1e21cedf2e84996d767b30f5b93f21f9
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-04-21 19:27:05 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,27 +2,44 @@
 </div>
 </div>
 
-<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
-<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<!-- DataTables JS -->
-<script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js') ?>" rel="stylesheet"></script>
-<script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js') ?>" rel="stylesheet"></script>
-<script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js');?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+<script src="<?= base_url('assets/js/sb-admin-2.min.js');?>"></script>
+<script src="<?= base_url('assets/vendor/chart.js/Chart.min.js');?>"></script>
+<script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js')?>"></scrip>
+<script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js')?>"></script>
 
 <script>
-    $(document).ready(function() {
-    $('#dataTable').DataTable({
-        "language": {
-            "search": "Cari:",
-            "lengthMenu": "Tampilkan _MENU_ data",
-            "info": "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
-            "paginate":{
-                "previous": "sebelumnya",
-                "next": "Berikutnya"
+    var ctx = document.getElementById('chartDashboard');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Kategori', 'Buku'],
+            datasets: [{
+                label: 'Jumlah Data',
+                data: [<?= $total_kategori; ?>, <?= $total_Buku; ?>],
+                backgroundColor: [
+                    '#4e73df',
+                    '#1cc88a'
+                ],
+                borderColor: [
+                    '#4e73df',
+                    '#1cc88a'
+                ],
+                
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
             }
         }
     });
-});
 </script>
 </body>
 </html>
