e6d9e0681b0a98ceabed5bdc98f7234bb122ee53
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-05-10 19:39:48 +0700

    Update Kategori_model.php

--- application/models/Kategori_model.php
+++ application/models/Kategori_model.php
@@ -1,29 +1,33 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class kategori_model extends CI_Model {
+class Kategori_model extends CI_Model{
 
-    private $table ='kategori';
+    private $table = 'kategori';
 
+    //Ambil semua data
     public function get_all()
     {
         return $this->db->get($this->table)->result();
-    }
-    public function insert($data)
-    {
-        return $this->db->insert($this->table, $data);
     }
     public function get_by_id($id)
     {
         $this->db->where('id',$id);
         return $this->db->get('kategori')->row();
     }
+    // Insert data
+    public function insert($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
     public function delete($id)
     {
-        return $this->db->delete($this->table,['id'=>$id]);
+        return $this->db->delete($this->table, ['id'=>$id]);
     }
-    public function update($id, $data)
+    public function update($id,$data)
     {
-        $this->db->where('id',$id);
+        $this->db->where('id', $id);
         return $this->db->update($this->table, $data);
     }
+
 }
