7184d0a8b62c36ff66d8cb3723d365a9fca8594f
nazwafauziahqq-rgb <nazwafauziahqq@gmail.com>
2026-05-10 19:39:02 +0700

    Update buku_model.php

--- application/models/buku_model.php
+++ application/models/buku_model.php
@@ -1,31 +1,45 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class buku_model extends CI_Model {
 
-    public function get_all(){
-        $this->db->select('buku.*, kategori.nama_kategori as nama_kategori');
-        $this->db->from('buku');
-        $this->db->join('kategori', 'kategori.id = buku.kategori_id');
-        return $this->db->get()->result();
+    private $table = 'buku';
+
+    // Ambil semua data buku (untuk DataTables nanti)
+    public function get_all()
+    {
+        return $this->db->get($this->table)->result();
     }
 
-    public function get_kategori(){
+    // Ambil data buku berdasarkan id
+    public function get_by_id($id)
+    {
+        $this->db->where('id_buku', $id);
+        return $this->db->get($this->table)->row();
+    }
+
+    // Insert data buku
+    public function insert($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+
+    // Update data buku
+    public function update($id, $data)
+    {
+        $this->db->where('id_buku', $id);
+        return $this->db->update($this->table, $data);
+    }
+
+    // Delete data buku
+    public function delete($id)
+    {
+        return $this->db->delete($this->table, ['id_buku' => $id]);
+    }
+
+    // Ambil kategori (untuk dropdown form)
+    public function get_kategori()
+    {
         return $this->db->get('kategori')->result();
-    }
-
-    public function insert($data){
-        return $this->db->insert('buku', $data);
-    }
-
-    public function get_by_id($id){
-        return $this->db->get_where('buku',['id'=>$id])->row();
-    }
-
-    public function update($id,$data){
-        $this->db->where('id',$id);
-        return $this->db->update('buku',$data);
-    }
-
-    public function delete($id){
-        return $this->db->delete('buku',['id'=>$id]);
     }
 }
